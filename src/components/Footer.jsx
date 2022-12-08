6eb2b9ef0aae4b43f50c23413b63cf0b89707883
Tab-Y <tlyoumans@gmail.com>
2022-12-07 19:43:13 -0500

    touch up for parameters

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,18 +1,22 @@
 import React from 'react';
 import './Footer.css'
 
-const Footer = ({ currentPage, handlePageChange }) => {
+const Footer = () => {
+    const additionalProfile = (e) => {
+        e.preventDefault();
+        alert(`Sorry. I don't have another professional profile currently.\nHopefully I'll create a StackOverflow account soon.`)
+    }
     return (
         <footer className='footer fixed-bottom'>
             <ul className='nav justify-content-center align-center'>
                 <li className='nav-item '>
-                    <a href="#" className='nav-link footerBtn'>github</a>
+                    <a href="https://github.com/Tab-Y" className='nav-link footerBtn'>github</a>
                 </li>
                 <li className='nav-item'>
-                    <a href="#" className='nav-link footerBtn'>linkedIn</a>
+                    <a href="https://www.linkedin.com/in/taber-youmans-500587131/" className='nav-link footerBtn'>linkedIn</a>
                 </li>
                 <li className='nav-item'>
-                    <a href="#" className='nav-link footerBtn'>Another Profile</a>
+                    <a onClick={additionalProfile} className='nav-link footerBtn'>Another Profile</a>
                 </li>
             </ul>
         </footer>
