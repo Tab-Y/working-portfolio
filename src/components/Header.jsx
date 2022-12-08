ad25f12c20801474b7f54a881d712811ab588bae
Tab-Y <tlyoumans@gmail.com>
2022-12-07 21:13:53 -0500

    fix mobile issues

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,19 +8,19 @@ const Header = ({ renderSplash, renderBio, renderProfile, renderProjects, render
     return (
         <header className='row header'>
             <h1 className='titleHeader col align-middle'><a onClick={renderSplash} className={active === '' ? 'active' : ''}>Taber Youmans</a></h1>
-            <nav className='col'>
-                <ul className='nav '>
+            <nav className='col-s-12 col-6'>
+                <ul className='nav'>
                     <li className='nav-item mx-auto'>
-                        <button className='navBtn'><a onClick={renderBio} id='navAboutMe' className={active === 'bio' ? 'navBtn active' : 'navBtn'}>About Me</a></button>
+                        <a onClick={renderBio} id='navAboutMe' className={active === 'bio' ? 'navBtn active' : 'navBtn'}>About Me</a>
                     </li>
                     <li className='nav-item mx-auto'>
-                        <button className='navBtn'><a onClick={renderProjects} id='navPortfolio' className={active === 'projects' ? 'navBtn active' : 'navBtn'}>Portfolio</a></button>
+                        <a onClick={renderProjects} id='navPortfolio' className={active === 'projects' ? 'navBtn active' : 'navBtn'}>Portfolio</a>
                     </li>
                     <li className='nav-item mx-auto'>
-                        <button className='navBtn'><a onClick={renderProfile} id='navContact' className={active === 'profile' ? 'navBtn active' : 'navBtn'}>Contact Me</a></button>
+                        <a onClick={renderProfile} id='navContact' className={active === 'profile' ? 'navBtn active' : 'navBtn'}>Contact Me</a>
                     </li>
                     <li className='nav-item mx-auto'>
-                        <button className='navBtn'><a onClick={renderResume} id='navResume' className={active === 'resume' ? 'navBtn active' : 'navBtn'}>Resume</a></button>
+                        <a onClick={renderResume} id='navResume' className={active === 'resume' ? 'navBtn active' : 'navBtn'}>Resume</a>
                     </li>
                 </ul>
             </nav>
